af320b25496303f8dd943e9a31e41f7e85037136
MrAngad <angad.97@protonmail.com>
2023-12-11 16:38:57 +0530

    formatting

--- contracts/StakePoolStorage.sol
+++ contracts/StakePoolStorage.sol
@@ -6,37 +6,48 @@ import "@nstbl-acl-manager/contracts/IACLManager.sol";
 import "./IStakePool.sol";
 
 contract StakePoolStorage {
+
+    /*//////////////////////////////////////////////////////////////
+    STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
     struct StakerInfo {
         uint256 amount;
         uint256 poolDebt;
         uint256 stakeTimeStamp;
         uint256 epochId;
     }
+
     /*//////////////////////////////////////////////////////////////
     IMMUTABLES
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice address of USDC token
     address public immutable usdc;
     uint256 internal immutable REVISION = 1;
 
     /*//////////////////////////////////////////////////////////////
-    STORAGE : Stake Pool
+    State Variables
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice version of the stake pool
     uint256 public versionSlot;
+
+    /// @notice address of the ACL manager
     address public aclManager;
+
+    /// @notice address of the NSTBL token
     address public nstbl;
+
+    /// @notice address of the loan manager
     address public loanManager;
+
+    /// @notice address of the ATVL
     address public atvl;
 
     uint256 public genesis;
 
-    uint64 public trancheFee1;
-    uint64 public trancheFee2;
-    uint64 public trancheFee3;
-
-    mapping(uint8 => mapping(address => StakerInfo)) public stakerInfo;
-
+    // Pool Parameters
     uint256 public poolProduct;
     uint256 public poolBalance;
     uint256 public poolEpochId;
@@ -44,17 +55,31 @@ contract StakePoolStorage {
 
     uint256 public oldMaturityVal;
     
+    /// @notice base fee for tranche 1
     uint32 public trancheBaseFee1;
+
+    /// @notice base fee for tranche 2
     uint32 public trancheBaseFee2;
+
+    /// @notice base fee for tranche 3
     uint32 public trancheBaseFee3;
 
+    /// @notice early unstake fee for tranche 1
     uint32 public earlyUnstakeFee1;
+
+    /// @notice early unstake fee for tranche 2
     uint32 public earlyUnstakeFee2;
+
+    /// @notice early unstake fee for tranche 3
     uint32 public earlyUnstakeFee3;
 
+    /// @notice mapping to store the tranche stake time periods
     mapping(uint8 => uint64) public trancheStakeTimePeriod;
 
-    //add new variables here to extended the storage
-    //reduce the gap size equal the size of new variables: to maintain original layout and prevent collision
+    mapping(uint8 => mapping(address => StakerInfo)) public stakerInfo;
+
+    // add new variables here to extended the storage
+
+    // reduce the gap size equal the size of new variables: to maintain original layout and prevent collision
     uint256[35] __gap;
 }
